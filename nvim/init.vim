9a1d2a25c8f8acd60de583eebd7f5be195636c90
Łukasz Daniluk <lukasz.dan@gmail.com>
2024-08-12 10:35:36 +0200

    Add initial configs
    Signed-off-by: Łukasz Daniluk <lukasz.dan@gmail.com>

---
+++ nvim/init.vim
@@ -0,0 +1,103 @@
+" Specify a directory for plugins
+" - For Neovim: stdpath('data') . '/plugged'
+" - Avoid using standard Vim directory names like 'plugin'
+call plug#begin(stdpath('data') . '/plugged')
+
+" Plug 'ibhagwan/fzf-lua', {'branch': 'main'}
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+Plug 'nvim-telescope/telescope.nvim', {'rev': '0.1.x'}
+Plug 'nvim-tree/nvim-web-devicons'
+Plug 'nvim-tree/nvim-tree.lua'
+Plug 'tpope/vim-fugitive'
+Plug 'nvim-lualine/lualine.nvim'
+Plug 'RRethy/nvim-base16'
+Plug 'cormacrelf/dark-notify'
+Plug 'neovim/nvim-lspconfig'
+
+Plug 'github/copilot.vim'
+
+
+Plug 'tpope/vim-abolish'
+
+Plug 'onsails/lspkind.nvim'
+
+
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-nvim-lsp-signature-help'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-vsnip'
+Plug 'hrsh7th/vim-vsnip'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+
+Plug 'akinsho/bufferline.nvim', { 'tag': 'v4.*' }
+
+Plug 'mfussenegger/nvim-dap'
+Plug 'theHamsta/nvim-dap-virtual-text'
+Plug 'mfussenegger/nvim-dap-python'
+
+" Plug 'simrat39/symbols-outline.nvim'
+Plug 'stevearc/aerial.nvim'
+
+Plug 'ruifm/gitlinker.nvim'
+Plug 'FeiyouG/commander.nvim'
+
+
+" Plug 'puremourning/vimspector'
+Plug 'varnishcache-friends/vim-varnish'
+
+Plug 'duane9/nvim-rg'
+
+Plug 'dstein64/vim-startuptime'
+
+Plug 'aznhe21/actions-preview.nvim'
+
+Plug 'CopilotC-Nvim/CopilotChat.nvim', { 'branch': 'canary' }
+
+" Plug 'neoclide/coc.nvim', {'branch': 'release'}
+
+call plug#end()
+
+let g:vimspector_base_dir='/Users/ldaniluk/.local/share/nvim/plugged/vimspector'
+
+set cul
+"set cuc
+set nu
+set ignorecase
+set smartcase
+set termguicolors
+set laststatus=3
+set signcolumn=yes
+
+lua require('init_vim')
+
+
+set spell spelllang=en_us
+
+let g:mapleader = "\\"
+nmap <Space> \
+
+nnoremap <Leader>h     <CMD>noh<CR>
+nnoremap <Leader>t     <CMD>NvimTreeToggle<CR>
+nnoremap <Leader>]     <CMD>Telescope lsp_references<CR>
+nnoremap <Leader>f     <CMD>Telescope find_files<CR>
+nnoremap <Leader>/     <CMD>Telescope live_grep<CR>
+nnoremap <Leader>[     <CMD>Telescope grep_string<CR>
+nnoremap <Leader><Tab> <CMD>Telescope buffers<CR>
+nnoremap <Leader>-     <CMD>Telescope diagnostics<CR>
+nnoremap <Leader>e     <CMD>lua vim.diagnostic.open_float(0, {scope="line"})<CR>
+nnoremap <Leader><CR>  <CMD>lua require("actions-preview").code_actions()<CR>
+
+nnoremap <Leader>sv    <CMD>source $MYVIMRC<CR>
+
+nnoremap <C-P>         <CMD>Telescope commander<CR>
+vnoremap <C-P>         <CMD>Telescope commander<CR>
+
+imap <silent><script><expr> <C-Space> copilot#Accept("")
+let g:copilot_no_tab_map = v:true
+let g:copilot_workspace_folders = ['~/repos/']
+
+" SymbolsOutlineOpen
+" NvimTreeOpen
---
+++ nvim/lua/init_vim.lua
@@ -0,0 +1,290 @@
+-- nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require('nvim-treesitter.configs').setup({
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+})
+
+require("nvim-tree").setup({
+	update_focused_file = { enable = true },
+	view = { width = 50 },
+})
+
+
+require('dark_notify').run({
+	schemes = {
+		dark = 'base16-tomorrow-night-eighties',
+		light = 'base16-atelier-heath-light',
+	}
+})
+
+require("bufferline").setup()
+
+local cmp_kinds = {
+	Text = '',
+	Method = '',
+	Function = '',
+	Constructor = '',
+	Field = '',
+	Variable = '',
+	Class = '',
+	Interface = '',
+	Module = '',
+	Property = '',
+	Unit = '',
+	Value = '',
+	Enum = '',
+	Keyword = '',
+	Snippet = '',
+	Color = '',
+	File = '',
+	Reference = '',
+	Folder = '',
+	EnumMember = '',
+	Constant = '',
+	Struct = '',
+	Event = '',
+	Operator = '',
+	TypeParameter = '',
+}
+
+
+local lsp = require('lspconfig')
+
+
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+local cmp = require('cmp')
+cmp.setup({
+	matching = {
+		disallow_partial_fuzzy_matching = false,
+		disallow_fuzzy_matching = false,
+		disallow_prefix_unmatching = false,
+	},
+	formatting = {
+		fields = { "kind", "abbr", "menu" },
+		format = function(entry, vim_item)
+			local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+			local strings = vim.split(kind.kind, "%s", { trimempty = true })
+			kind.kind = " " .. (strings[1] or "") .. " "
+			kind.menu = "    (" .. (strings[2] or "") .. ")"
+			kind.menu = strings[3]
+
+			return kind
+		end,
+	},
+	snippet = {
+		expand = function (args)
+			vim.fn["vsnip#anonymous"](args.body)
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<CR>'] = cmp.mapping.confirm({ select = false }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif vim.fn["vsnip#available"](1) == 1 then
+				feedkey("<Plug>(vsnip-expand-or-jump)", "")
+			elseif has_words_before() and cmp.visible() then
+				cmp.complete()
+			else
+				fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+			end
+		end, { "i", "s" }),
+
+		["<S-Tab>"] = cmp.mapping(function()
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+				feedkey("<Plug>(vsnip-jump-prev)", "")
+			end
+		end, { "i", "s" }),
+	}),
+	sources = {
+		{ name = 'nvim_lsp' },
+		{ name = 'nvim_lsp_signature_help' },
+		{ name = 'buffer' },
+		{ name = 'vsnip' },
+		{ name = 'path' },
+	},
+	-- sources = cmp.config.sources({
+	-- 	{ name = 'nvim_lsp' },
+	-- 	{ name = 'nvim_lsp_signature_help' },
+	-- 	{ name = 'buffer' },
+	-- 	{ name = 'vsnip' },
+	-- 	{ name = 'path' },
+	-- }),
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' }
+		, 
+		{ name = 'cmdline' }
+	})
+})
+
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+lsp.pyright.setup({
+	capabilities = capabilities,
+	settings = {
+		python = {
+			venvPath = "/Users/ldaniluk/.pyenv/versions/",
+			venv = vim.fn.fnamemodify(vim.fn.getcwd(), ":t"),
+		}
+	},
+	-- cmd = {
+	-- 	"pyright-langserver", "--stdio", "--venvPath=/Users/ldaniluk/.pyenv/versions/", "--venv=" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+	-- }
+})
+
+lsp.terraformls.setup({})
+
+lsp.rust_analyzer.setup({
+    on_attach=on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+	    diagnostics = {
+		enable = true,
+		experimental = { enable = true, },
+	    },
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+	    inlay_hint = { enable = true, },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+})
+
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+vim.lsp.handlers.signature_help, {
+	border = 'rounded',
+	close_events = {"CursorMoved", "BufHidden", "InsertCharPre"},
+}
+)
+
+
+require("nvim-dap-virtual-text").setup()
+require('dap-python').setup()
+-- require('symbols-outline').setup()
+require("aerial").setup({
+  open_automatic = false,
+  layout = {
+    min_width = { 40, 0.2 },
+    max_width = { 60, 0.2 },
+  },
+  filter_kind = {
+  -- defaults
+    "Class",
+    "Constructor",
+    "Enum",
+    "Function",
+    "Interface",
+    "Module",
+    "Method",
+    "Struct",
+  -- additional
+    "Constant",
+    "Variable",
+  },
+})
+
+require('lualine').setup({
+    sections = {
+	lualine_c = {
+		"filename", "aerial",
+	},
+    },
+})
+
+
+require('gitlinker').setup({
+	mappings = false,
+})
+
+local command_center = require("commander")
+command_center.setup({
+   components = {"DESC", "KEYS"},
+   sort_by = { "DESC" }
+})
+command_center.add({{
+   desc = "get github url to current line",
+   cmd = '<CMD>lua require"gitlinker".get_buf_range_url()<CR>',
+   keys = {"n", "<Leader>p"}
+}})
+command_center.add({{
+   desc = "black",
+   cmd = '<CMD>!black %<CR>',
+   keys = {"n", "<Leader>b"}
+}})
+command_center.add({{
+   desc = "coerce to upper",
+   cmd = 'cru',
+}})
+
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client.server_capabilities.inlayHintProvider then
+            vim.lsp.inlay_hint.enable(true)
+        end
+        -- whatever other lsp config you want
+    end
+})
+
+require("actions-preview").setup {
+  telescope = {
+    sorting_strategy = "ascending",
+    layout_strategy = "vertical",
+    layout_config = {
+      width = 0.8,
+      height = 0.9,
+      prompt_position = "top",
+      preview_cutoff = 20,
+      preview_height = function(_, _, max_lines)
+        return max_lines - 15
+      end,
+    },
+  },
+}
